44bdaf38c7f5038b476040d25a4f9e8a2d63159b
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2023-11-05 23:39:31 +0300

    feat: Implement composition root of application

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 
-var(
-	Version string
-	Commit string
+	"github.com/mehmetumit/dexus/internal/adapters/fileredirect"
+	"github.com/mehmetumit/dexus/internal/adapters/memcache"
+	"github.com/mehmetumit/dexus/internal/adapters/rest"
+	"github.com/mehmetumit/dexus/internal/adapters/stdlog"
+	"github.com/mehmetumit/dexus/internal/core/app"
 )
 
-func main(){
-	fmt.Printf("Hello dexus\nVersion: %s | Commit: %s\n", Version, Commit)
+var (
+	Version string
+	Commit  string
+)
+
+// Composition root
+func main() {
+	//TODO env
+	debugLevel := true
+	logger := stdlog.NewStdLog(debugLevel)
+	logger.Info(fmt.Sprintf("Version: %s | Commit: %s", Version, Commit))
+	cacher := memcache.NewMemCache(logger)
+	fPath := filepath.FromSlash("test_data/redirection.yaml")
+	redirectRepo, err := fileredirect.NewYamlRedirect(logger, fPath)
+	if err != nil {
+		os.Exit(1)
+	}
+	appCore := app.NewApp(app.AppConfig{
+		Logger:          logger,
+		RedirectionRepo: redirectRepo,
+	})
+	rest.NewServer(logger, &rest.ServerConfig{
+		Addr: "localhost:8080",
+		//IdleTimeout:  90 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}, appCore, rest.WithCacher(cacher, logger, 5*time.Second))
 }
